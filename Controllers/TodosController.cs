825f11a4b736b5f5f1f8fcfe03d7d3e8947bf72c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-12-04 18:15:14 -0800

    works well

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -4,21 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 using Mongo.NetCoreWebAPI.Models;
 using Mongo.NetCoreWebAPI.Services;
 
-namespace Mongo.NetCoreWebAPI.Controllers{
-    private var mogoDbService = new MongoDBService("", "" ,"");
+namespace Mongo.NetCoreWebAPI.Controllers
+{
     [RouteAttribute("api/[controller]")]
-    public class TodoController : Controller
+    public class TodosController : Controller
     {
-        [HttpGetAttribute]
+        private MongoDBService mogoDbService = new MongoDBService("mongodb://user:user123@ds119618.mlab.com:19618/db_todo", "db_todo", "todos");
+
+        [HttpGet]
         public async Task<JsonResult> Get()
         {
             var allTodos = await mogoDbService.GetAllTodos();
             return Json(allTodos);
         }
 
-        [HttpPostAttribute]
+        [HttpPost]
         public async Task Post([FromBody]TodoModel todo)
         {
+            // To force Web API to read a simple type from the request body, 
+            // add the [FromBody] attribute to the parameter
             await mogoDbService.InsertTodo(todo);
         }
     }
--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # MongoDB and .NET core Web API
+Reference:
+    Dotnet Core - MongoDb - A simple Web API
+
+
+## Overview
+* Controller -> Service -> Model    
+
 
 ## How to
 * Add MongoDB.Driver v2.3.0 to project.json
+* Add model
+* Add servcie
+* Add Controller
 
 
-
-
-
-*   [Add]
-
-## Overview
-
-## Run & Deploy
+## How to test
+* GET
+    Content-Type : application/json
+    localhost:5000/api/todos
+    
+* POST
+    Content-Type : application/json   // Set it in postman or other client application
+    localhost:5000/api/todos
+    {"title":"aa", }
